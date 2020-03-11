6915c07596156e7b89f58687e41405c9c5d59652
zaanposni <zaanposni@users.noreply.github.com>
2020-03-11 15:40:27 +0100

    added first version of docs

--- ChatMail/ChatGUI.cs
+++ ChatMail/ChatGUI.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace ChatMail
 {
-    public partial class Form1 : Form
+    public partial class ChatGUI : Form
     {
-        public Form1()
+        public ChatGUI()
         {
             InitializeComponent();
         }
