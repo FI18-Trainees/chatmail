acb9cad6e678578d151165cb1b4f93df5aa9dc69
zaanposni <zaanposni@users.noreply.github.com>
2020-03-27 15:29:46 +0100

    introduced logger

---
+++ ChatMail/Logging/LogEntry.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatMail.Logging
+{
+    public class LogEntry
+    {
+        public string Content { get; }
+        public string Origin { get; }
+        public DateTime Timestamp { get; }
+        public LogLevel LogLevel { get;  }
+
+        /// <summary>
+        /// Constructor of LogEntry
+        /// </summary>
+        /// <param name="content">Body of message</param>
+        /// <param name="origin">Where this message came from (e.g. ChatMail.Frontend)</param>
+        /// <param name="logLevel">LogLevel of this message</param>
+        public LogEntry(string content, string origin, LogLevel logLevel)
+        {
+            if (!content.StartsWith("ChatMail"))
+            {
+                content = "ChatMail." + content;
+            }
+            this.Content = content;
+            this.Origin = origin;
+            this.Timestamp = DateTime.Now;
+            this.LogLevel = logLevel;
+        }
+
+        /// <summary>
+        /// Displays human readable version of logentry
+        /// </summary>
+        /// <returns>hum readable string</returns>
+        public string display()
+        {
+            string time = this.Timestamp.ToString("yyyy.MM.dd HH:mm:ss");
+            return "[" + this.LogLevel.ToString() + "] [" + time + "] " + this.Origin + ": " + this.Content;
+        }
+    }
+}
---
+++ ChatMail/Logging/Logger.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatMail.Logging
+{
+    public static class Logger
+    {
+        public static List<LogEntry> LogEntries = new List<LogEntry>();
+        public static bool logToDebug { get; set; } = false;
+        public static LogLevel loggerLevel { get; set; } = LogLevel.Info;
+
+        /// <summary>
+        /// Creates logentry and adds it to LogEntries and displays message if needed
+        /// </summary>
+        /// <param name="content">Body of message</param>
+        /// <param name="origin">Where this message came from (e.g. ChatMail.Frontend)</param>
+        /// <param name="logLevel">LogLevel of this message</param>
+        private static void log(string content, string origin = "ChatMail", LogLevel logLevel = LogLevel.Debug)
+        {
+            LogEntry newEntry = new LogEntry(content, origin, logLevel);
+            LogEntries.Add(newEntry);
+
+            if (logToDebug && loggerLevel <= newEntry.LogLevel)
+            {
+                Debug.WriteLine(newEntry.display());
+            }
+        }
+
+        /// <summary>
+        /// Creates logentry with LogLevel Debug
+        /// </summary>
+        /// <param name="content">Body of message</param>
+        /// <param name="origin">Where this message came from (e.g. ChatMail.Frontend)</param>
+        public static void debug(string content, string origin = "ChatMail")
+        {
+            log(content, origin, LogLevel.Debug);
+        }
+        /// <summary>
+        /// Creates logentry with LogLevel Info
+        /// </summary>
+        /// <param name="content">Body of message</param>
+        /// <param name="origin">Where this message came from (e.g. ChatMail.Frontend)</param>
+        public static void info(string content, string origin = "ChatMail")
+        {
+            log(content, origin, LogLevel.Info);
+        }
+        /// <summary>
+        /// Creates logentry with LogLevel Warning
+        /// </summary>
+        /// <param name="content">Body of message</param>
+        /// <param name="origin">Where this message came from (e.g. ChatMail.Frontend)</param>
+        public static void warning(string content, string origin = "ChatMail")
+        {
+            log(content, origin, LogLevel.Warning);
+        }
+        /// <summary>
+        /// Creates logentry with LogLevel Critical
+        /// </summary>
+        /// <param name="content">Body of message</param>
+        /// <param name="origin">Where this message came from (e.g. ChatMail.Frontend)</param>
+        public static void critical(string content, string origin = "ChatMail")
+        {
+            log(content, origin, LogLevel.Critical);
+        }
+    }
+}
