f59df0c2d3963db119f1ca89d2505eb7e5ea679b
zaanposni <zaanposni@users.noreply.github.com>
2020-03-27 15:33:12 +0100

    fixed sqlselect

--- ChatMail/SQL/selectMessageByRId.sql
+++ ChatMail/SQL/selectMessageByRId.sql
@@ -1,1 +1,1 @@
-SELECT message.mId, content, timestamp, senderId FROM chatmail.message INNER JOIN chatmail.messagereceiver ON (message.mId = messagereceiver.mId AND messagereceiver.rId = @RID);
+SELECT message.mId, content, timestamp, senderId FROM chatmail.message INNER JOIN chatmail.messagereceiver ON message.mId = messagereceiver.mId WHERE messagereceiver.rId = @RID;
